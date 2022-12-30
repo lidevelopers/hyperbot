dbea07eca5cee79545b50c7949c56a2f06f90fd8
lidevelopers <85686806+lidevelopers@users.noreply.github.com>
2022-12-30 22:12:27 +0530

    Update Dockerfile.sh

--- Dockerfile.sh
+++ Dockerfile.sh
@@ -13,3 +13,9 @@ yarn install
 echo "installing 7z"
 
 sudo apt-get install p7zip-full -y
+
+RUN apt-get update && apt-get upgrade -y && apt-get install -y wget unzip python3 python3-pip
+
+RUN wget https://abrok.eu/stockfish/latest/linux/stockfish_x64_bmi2.zip -O stockfish.zip
+RUN unzip stockfish.zip && rm stockfish.zip
+RUN mv stockfish13_* engines/stockfish13
